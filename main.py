b6ab0deac6b5d765c483494614163a0a91fb6568
Shashank <bruteforce266@gmail.com>
2026-01-17 19:48:07 +0530

    Zero downtime

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello from inside the Lunchbox!"
+    return "Welcome to VERSION 2! 🚀"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
